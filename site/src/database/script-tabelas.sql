9386ab2103250c58718fe86d60021a850347a01d
Homero Brescancin <homero.passos@sptech.school>
2025-05-13 11:31:02 -0300

    API

---
+++ site/src/database/script-tabelas.sql
@@ -0,0 +1,20 @@
+create database carmod;
+use carmod;
+
+CREATE TABLE usuario (
+    id INT PRIMARY KEY auto_increment,
+    Nome VARCHAR(45),
+    email VARCHAR(45),
+     senha VARCHAR(45),
+    Estado VARCHAR(45)
+);
+
+select * from usuario;
+
+
+
+
+
+
+
+
